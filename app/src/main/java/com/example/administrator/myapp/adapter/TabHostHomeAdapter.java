363b168dbe1cd28a3582cc6a154e2a3c2b2a6931
ggkb <294446993@qq.com>
2017-10-11 09:09:02 +0800

    second

---
+++ app/src/main/java/com/example/administrator/myapp/adapter/TabHostHomeAdapter.java
@@ -0,0 +1,10 @@
+package com.example.administrator.myapp.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+
+public class TabHostHomeAdapter extends RecyclerView.Adapter<TabHostHomAdapter.> {
+}
---
+++ app/src/main/java/com/example/administrator/myapp/bean/TabHostHomeDataBean.java
@@ -0,0 +1,8 @@
+package com.example.administrator.myapp.bean;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+
+public class TabHostHomeDataBean {
+}
